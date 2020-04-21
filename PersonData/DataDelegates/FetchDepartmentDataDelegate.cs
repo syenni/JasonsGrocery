aa6a7f7fafa80e6ade78afb7be69313cf1a248e3
benjaminh1234 <benjaminh1234@yahoo.com>
2020-04-21 08:30:26 -0500

    Edits to DataDelegates and IRepository

---
+++ PersonData/DataDelegates/FetchDepartmentDataDelegate.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess;
+using StoreData.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DepartmentData.DataDelegates
+{
+   internal class FetchDepartmentDataDelegate : DataReaderDelegate<Department>
+   {
+      private readonly int DepartmentID;
+
+      public FetchDepartmentDataDelegate(int DepartmentID)
+         : base("Department.FetchDepartment")
+      {
+         this.DepartmentID = DepartmentID;
+      }
+
+      public override void PrepareCommand(SqlCommand command)
+      {
+         base.PrepareCommand(command);
+
+         var p = command.Parameters.Add("DepartmentID", SqlDbType.Int);
+         p.Value = DepartmentID;
+      }
+
+      public override Department Translate(SqlCommand command, IDataRowReader reader)
+      {
+         if (!reader.Read())
+            throw new RecordNotFoundException(DepartmentID.ToString());
+
+            return new Department(
+               DepartmentID,
+               reader.GetValue<DepartmentType>("DepartmentType"),
+               reader.GetInt32("StoreID"),
+               reader.GetString("DepartmentName"));
+        }
+   }
+}
---
+++ PersonData/DataDelegates/GetDepartmentDataDelegate.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess;
+using StoreData.Models;
+using System.Data.SqlClient;
+
+namespace DepartmentData.DataDelegates
+{
+   internal class GetDepartmentDataDelegate : DataReaderDelegate<Department>
+   {
+      private readonly string email;
+
+      public GetDepartmentDataDelegate(string email)
+         : base("Department.GetDepartment")
+      {
+         this.email = email;
+      }
+
+      public override void PrepareCommand(SqlCommand command)
+      {
+         base.PrepareCommand(command);
+
+         command.Parameters.AddWithValue("Email", email);
+      }
+
+      public override Department Translate(SqlCommand command, IDataRowReader reader)
+      {
+         if (!reader.Read())
+            return null;
+
+            return new Department(
+               reader.GetInt32("DepartmentID"),
+               reader.GetValue<DepartmentType>("DepartmentType"),
+               reader.GetInt32("StoreID"),
+               reader.GetString("DepartmentName"));
+      }
+   }
+}
