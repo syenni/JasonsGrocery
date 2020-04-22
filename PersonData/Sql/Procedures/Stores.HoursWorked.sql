4f65527b3b2cf1d76d14484b93299c5d844cf947
benjaminh1234 <benjaminh1234@yahoo.com>
2020-04-22 15:29:55 -0500

    New HoursWorked Report Query

--- PersonData/Sql/Procedures/Stores.HoursWorked.sql
+++ PersonData/Sql/Procedures/Stores.HoursWorked.sql
@@ -1,4 +1,8 @@
 ﻿--4R: list of employees who worked most hours in specific month
+CREATE OR ALTER PROCEDURE Stores.ItemsUnderCertainPrice
+   @EmployeeID INT,
+   @Month DATETIME
+AS
 SELECT E.EmployeeName, 
 	SUM(DATEDIFF(
 		HOUR,
@@ -7,5 +11,5 @@ SELECT E.EmployeeName,
 	)) AS HoursWorked
 FROM Stores.Employee E
 	INNER JOIN Stores.TimeEntry TE ON TE.EmployeeID = E.EmployeeID
-WHERE MONTH(TE.ClockIn) = 04
+WHERE MONTH(TE.ClockIn) = @Month AND E.EmployeeID = @EmployeeID
 GROUP BY E.EmployeeName;
