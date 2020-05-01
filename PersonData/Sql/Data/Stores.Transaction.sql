5530200fa3131bf7010e446fd153ff42cc0853a0
Jason Byrne <jjbyrne@ksu.edu>
2020-04-30 22:00:08 -0500

    transactions

--- PersonData/Sql/Data/Stores.Transaction.sql
+++ PersonData/Sql/Data/Stores.Transaction.sql
@@ -7,7 +7,7 @@ SELECT S.StoreID, T.PurchasedOn
 FROM 
 	(
 		VALUES	
-			(N'Jason''s Grocery Store', '2020-04-28 05:30:00')
+			(N'Jason''s Grocery Store', '2020-03-15 08:30:00')
 	)T(StoreName, PurchasedOn)
 	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
 SELECT*
@@ -20,7 +20,7 @@ SELECT S.StoreID, T.PurchasedOn
 FROM 
 	(
 		VALUES	
-			(N'Jason''s Grocery Store', '2020-04-28 13:40:00')
+			(N'Jason''s Grocery Store', '2020-03-15 13:40:00')
 	)T(StoreName, PurchasedOn)
 	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
 SELECT*
@@ -32,7 +32,7 @@ SELECT S.StoreID, T.PurchasedOn
 FROM 
 	(
 		VALUES	
-			(N'Jason''s Grocery Store', '2020-04-29 12:00:00')
+			(N'Jason''s Grocery Store', '2020-03-15 16:00:00')
 	)T(StoreName, PurchasedOn)
 	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
 SELECT*
@@ -45,7 +45,7 @@ SELECT S.StoreID, T.PurchasedOn
 FROM 
 	(
 		VALUES	
-			(N'Jason''s Grocery Store', '2020-04-29 16:30:00')
+			(N'Jason''s Grocery Store', '2020-03-26 6:30:00')
 	)T(StoreName, PurchasedOn)
 	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
 SELECT*
@@ -57,7 +57,7 @@ SELECT S.StoreID, T.PurchasedOn
 FROM 
 	(
 		VALUES	
-			(N'Jason''s Grocery Store', '2020-04-30 08:30:00')
+			(N'Jason''s Grocery Store', '2020-03-26 08:30:00')
 	)T(StoreName, PurchasedOn)
 	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
 SELECT*
@@ -69,7 +69,199 @@ SELECT S.StoreID, T.PurchasedOn
 FROM 
 	(
 		VALUES	
-			(N'Jason''s Grocery Store', '2020-04-27 16:20:00')
+			(N'Jason''s Grocery Store', '2020-03-26 12:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 7
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-03-26 15:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 8
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-03-31 8:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 9
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-03-31 14:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 10
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-03-31 15:30:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 11
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-03-31 16:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 12
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-03-31 16:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 13
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-04 10:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 14
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-04 14:45:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 15
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-04 16:10:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 16
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-04 17:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 15
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-04 16:10:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 16
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-04 17:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 17
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-30 6:10:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 18
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-30 12:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 19
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-30 14:20:00')
+	)T(StoreName, PurchasedOn)
+	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
+SELECT*
+FROM Stores.[Transaction] T;
+
+--transaction 20
+INSERT Stores.[Transaction](StoreID, PurchasedOn)
+SELECT S.StoreID, T.PurchasedOn
+FROM 
+	(
+		VALUES	
+			(N'Jason''s Grocery Store', '2020-04-30 16:15:00')
 	)T(StoreName, PurchasedOn)
 	INNER JOIN Stores.Store S ON S.StoreName = T.StoreName;
 SELECT*
--- PersonData/Sql/Data/Stores.TransactionDetails.sql
+++ PersonData/Sql/Data/Stores.TransactionDetails.sql
@@ -147,3 +147,301 @@ FROM Stores.Product P
 	RIGHT JOIN Stores.TransactionDetails TD6 ON TD6.ProductID = P.ProductID
 	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD6.TransactionID
 WHERE T.TransactionID = 6;
+
+--Tranaction 7
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD7.UnitPrice, TD7.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Ham', 3*2, 2),
+			(N'Turkey', 3.25*1, 1)
+	) TD7(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD7.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 7;
+
+--update after transaction 7
+UPDATE P 
+SET	
+	StockQuantity -= TD7.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD7 ON TD7.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD7.TransactionID
+WHERE T.TransactionID = 7;
+
+--Tranaction 8
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD8.UnitPrice, TD8.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'California Roll', 2.50*2, 2),
+			(N'Apples', 3*10, 10)
+	) TD8(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD8.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 8;
+
+--update after transaction 8
+UPDATE P 
+SET	
+	StockQuantity -= TD8.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD8 ON TD8.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD8.TransactionID
+WHERE T.TransactionID = 8;
+
+--Tranaction 9
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD9.UnitPrice, TD9.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'California Roll', 2.50*5, 5),
+			(N'Dynamite Roll', 4.50*11, 11),
+			(N'Milk', 2*1, 1)
+	) TD9(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD9.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 9;
+
+--update after transaction 9
+UPDATE P 
+SET	
+	StockQuantity -= TD9.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD9 ON TD9.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD9.TransactionID
+WHERE T.TransactionID = 9;
+
+--Tranaction 10
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD10.UnitPrice, TD10.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Kiwis', 1.15*15, 15),
+			(N'Strawberry Ice Cream', 1.25*1, 1),
+			(N'Corn', 2.25*6, 5),
+			(N'Butter', 4*12, 12)
+	) TD10(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD10.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 10;
+
+--update after transaction 10
+UPDATE P 
+SET	
+	StockQuantity -= TD10.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD10 ON TD10.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD10.TransactionID
+WHERE T.TransactionID = 10;
+
+--Tranaction 11
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD11.UnitPrice, TD11.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Celery', 1*20, 20)
+	) TD11(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD11.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 11;
+
+--update after transaction 11
+UPDATE P 
+SET	
+	StockQuantity -= TD11.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD11 ON TD11.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD11.TransactionID
+WHERE T.TransactionID = 11;
+
+--Tranaction 12
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD12.UnitPrice, TD12.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Orange Sherbet', 1.75*20, 20),
+			(N'Cottage Cheese', 2.50*20, 20)
+	) TD12(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD12.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 12;
+
+--update after transaction 12
+UPDATE P 
+SET	
+	StockQuantity -= TD12.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD12 ON TD12.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD12.TransactionID
+WHERE T.TransactionID = 12;
+
+--Tranaction 13
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD13.UnitPrice, TD13.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Onions', 1.15*8, 8),
+			(N'Oranges', 2.25*14, 14)
+	) TD13(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD13.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 13;
+
+--update after transaction 13
+UPDATE P 
+SET	
+	StockQuantity -= TD13.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD13 ON TD13.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD13.TransactionID
+WHERE T.TransactionID = 13;
+
+--Tranaction 14
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD14.UnitPrice, TD14.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Broccoli', 2*20, 20),
+			(N'Chobani Yogurt', 2.50*10, 10)
+	) TD14(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD14.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 14;
+
+--update after transaction 14
+UPDATE P 
+SET	
+	StockQuantity -= TD14.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD14 ON TD14.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD14.TransactionID
+WHERE T.TransactionID = 14;
+
+--Tranaction 15
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD15.UnitPrice, TD15.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Ham', 3*5, 5),
+			(N'Banana Nut Muffins', 2.00*1, 1),
+			(N'Chocolate Chip Muffins', 1.75*1, 1),
+			(N'Blueberry Muffins', 1.50*1, 1)
+	) TD15(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD15.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 15;
+
+--update after transaction 15
+UPDATE P 
+SET	
+	StockQuantity -= TD15.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD15 ON TD15.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD15.TransactionID
+WHERE T.TransactionID = 15;
+
+--Tranaction 16
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD16.UnitPrice, TD16.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Sugar Cookies', 1.50*13, 13),
+			(N'Raisin Cookies', 1.00*1, 1),
+			(N'Orange Sherbet', 1.75*2, 2)
+	) TD16(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD16.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 16;
+
+--update after transaction 16
+UPDATE P 
+SET	
+	StockQuantity -= TD16.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD16 ON TD16.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD16.TransactionID
+WHERE T.TransactionID = 16;
+
+--Tranaction 17
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD17.UnitPrice, TD17.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Potatoes', 2.25*30, 30)
+	) TD17(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD17.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 17;
+
+--update after transaction 17
+UPDATE P 
+SET	
+	StockQuantity -= TD17.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD17 ON TD17.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD17.TransactionID
+WHERE T.TransactionID = 17;
+
+--Tranaction 18
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD18.UnitPrice, TD18.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Sour Cream', 3*30, 30),
+			(N'White Bread', 2*10, 10)
+	) TD18(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD18.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 18;
+
+--update after transaction 18
+UPDATE P 
+SET	
+	StockQuantity -= TD18.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD18 ON TD18.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD18.TransactionID
+WHERE T.TransactionID = 18;
+
+--Tranaction 19
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD19.UnitPrice, TD19.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Strawberries', 2*10, 10),
+			(N'Tomatoes', 1.50*10, 10),
+			(N'Yogurt', 3*10, 10)
+	) TD19(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD19.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 19;
+
+--update after transaction 19
+UPDATE P 
+SET	
+	StockQuantity -= TD19.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD19 ON TD18.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD19.TransactionID
+WHERE T.TransactionID = 19;
+
+--Tranaction 20
+INSERT Stores.TransactionDetails(TransactionID, ProductID, StoreID, UnitPrice, ItemQuantity)
+SELECT T.TransactionID, P.ProductID, P.StoreID, TD20.UnitPrice, TD20.ItemQuantity
+FROM 
+	(
+		VALUES
+			(N'Celery', 1*15, 15)
+	) TD20(ProductName, UnitPrice, ItemQuantity)
+	INNER JOIN Stores.Product P ON P.ProductName = TD20.ProductName
+	INNER JOIN Stores.[Transaction] T ON T.StoreID = P.StoreID AND T.TransactionID = 20;
+
+--update after transaction 20
+UPDATE P 
+SET	
+	StockQuantity -= TD20.ItemQuantity
+FROM Stores.Product P
+	RIGHT JOIN Stores.TransactionDetails TD20 ON TD18.ProductID = P.ProductID
+	INNER JOIN Stores.[Transaction] T ON T.TransactionID = TD20.TransactionID
+WHERE T.TransactionID = 20;
